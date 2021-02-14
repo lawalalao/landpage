067227a6dfc5d9313cc690d838ce651456fa761e
LawalAlao <lawalalaoad@gmail.com>
2021-02-14 23:35:50 +0100

    chage date

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const countdown = document.querySelector(".countdown");
 
 const interval = setInterval(() => {
-  const deadline = new Date(2021,02, 01, 00, 00, 00);
+  const deadline = new Date(2021,01, 21, 00, 00, 00);
 
   const current = new Date();
 
